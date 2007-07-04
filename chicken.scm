6c91a7ed14348dbdde48cdf3bc9fc7d445aedb97
Taylor R Campbell <campbell@mumble.net>
2007-07-04 17:36:52 +0000

    Fix typo in chicken.scm *again*: CDR-SELCTOR, not CDR-SELECTOR.  (How did this pop up??)

--- chicken.scm
+++ chicken.scm
@@ -274,7 +274,7 @@
     (lambda (define-classifier)
       (define-classifier '(DECLARE + (NAME * DATUM))
         (lambda (form environment history)
-          (values (make-declaration-definition cdr-selctor
+          (values (make-declaration-definition cdr-selector
                                                (cdr form)
                                                environment
                                                history)
